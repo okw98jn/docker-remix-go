606bb1cd61f6c0f9ed9c6a16108fa592187320cc
okawa <0cu2045d@gmail.com>
2023-12-29 14:10:35 +0900

    action仮

--- frontend/app/routes/todos.tsx
+++ frontend/app/routes/todos.tsx
@@ -1,19 +1,18 @@
-import { Outlet, useActionData } from '@remix-run/react'
+import { Outlet } from '@remix-run/react'
 import { FC } from 'react'
 import styles from '../styles/layout.module.css'
-import Header from '~/components/Header'
+import Header from '../components/Header'
 import { ActionFunction, json } from '@remix-run/node'
+import { createTodo } from '../../model/todo.server'
 
 export const action: ActionFunction = async ({ request }: { request: Request }) => {
-    const formData = await request.formData();
-    const data = Object.fromEntries(formData);
-    await new Promise(resolve => setTimeout(resolve, 500));
+    const todoId = await createTodo(request);
+    console.log(todoId);
+    
     return json({ message: 'success' });
 }
 
 const TodoLayout: FC = () => {
-    const data2 = useActionData();
-    console.log(data2);
     return (
         <div className={styles.wrap}>
             <div className={styles.flexColumn}>

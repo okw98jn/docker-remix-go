5cce69706a656b696f5e4f70395eefc0cb62768b
okawa <0cu2045d@gmail.com>
2023-12-30 00:30:29 +0900

    高さ指定修正

--- frontend/app/components/auth/Box.tsx
+++ frontend/app/components/auth/Box.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const Box: FC<Props> = ({ children }) => {
     return (
-        <div className={css({ bg: 'gray.100', display: 'flex', height: '100%', alignItems: 'center', justifyContent: 'center' })}>
+        <div className={css({ bg: 'gray.100', display: 'flex', height: '100vh', alignItems: 'center', justifyContent: 'center' })}>
             <div className={css({ bg: 'white', border: 'solid 1px', borderColor: 'gray.200', width: '1/4', padding: '8', borderRadius: 'md' })}>
                 {children}
             </div>

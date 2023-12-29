0d47bbdb7ff391c6158e7421217ff5b6c0c21867
okawa <0cu2045d@gmail.com>
2023-12-30 00:30:17 +0900

    サブミット中ボタン非活性

--- frontend/app/components/auth/Button.tsx
+++ frontend/app/components/auth/Button.tsx
@@ -1,13 +1,34 @@
 import { FC } from "react";
-import { css } from "../../../styled-system/css";
+import { useNavigation } from "@remix-run/react";
+import { cva } from "../../../styled-system/css";
 
 type Props = {
     text: 'ログイン' | '新規登録';
 }
 
+export const button = cva({
+    base: {
+        w: 'full', p: '2', borderRadius: 'sm'
+    },
+    variants: {
+        type: {
+            normal: { bg: 'blue.500', color: 'white', cursor: 'pointer', _hover: { opacity: '0.8' } },
+            loading: { bg: "#888888a9", color: "#ffffff" },
+        },
+    },
+});
+
 const Button: FC<Props> = ({ text }) => {
+    const navigation = useNavigation();
+    const isSubmitting = navigation.state === 'submitting';
+    const btnStyle = isSubmitting ? button({ type: "loading" }) : button({ type: "normal" });
     return (
-        <button type="submit" className={css({ w: 'full', p: '2', bg: 'blue.500', color: 'white', borderRadius: 'sm', cursor: 'pointer', _hover: { opacity: '0.8' } })}>{text}</button>
+        <button type="submit"
+            className={btnStyle}
+            disabled={isSubmitting}
+        >
+            {isSubmitting ? '送信中...' : text}
+        </button>
     );
 }
 

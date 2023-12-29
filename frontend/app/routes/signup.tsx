6eefcef237ebbc9bf2cfe163906ab9881bae053e
okawa <0cu2045d@gmail.com>
2023-12-30 00:30:52 +0900

    簡易的にaction記述

--- frontend/app/routes/signup.tsx
+++ frontend/app/routes/signup.tsx
@@ -1,4 +1,5 @@
 import { Form } from "@remix-run/react";
+import { ActionFunction } from "@remix-run/server-runtime";
 import Box from "../components/auth/Box";
 import Title from "../components/auth/Title";
 import Label from "../components/auth/Label";
@@ -6,11 +7,21 @@ import Input from "../components/auth/Input";
 import Button from "../components/auth/Button";
 import ItemBox from "../components/auth/ItemBox";
 
+export const action: ActionFunction = async ({ request }) => {
+	const formData = await request.formData();
+    const email = formData.get('email');
+    const password = formData.get('password');
+    await new Promise(resolve => setTimeout(resolve, 500));
+	return {
+		title: 'Sign in',
+	};
+}
+
 export default function SignUp() {
     return (
         <Box>
             <Title title={'Sign up'} subTitle="ログイン" path={'/login'} />
-            <Form>
+            <Form method="post">
                 <ItemBox>
                     <Label name="email" label="メールアドレス" />
                     <Input type="text" name="email" />

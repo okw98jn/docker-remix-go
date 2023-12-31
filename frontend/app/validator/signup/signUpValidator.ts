f789ea5f0fa8682f3108fc1317519c5d757ffb33
okawa <0cu2045d@gmail.com>
2023-12-31 17:50:04 +0900

    メールアドレスのバリデーション追加

--- frontend/app/validator/signup/signUpValidator.ts
+++ frontend/app/validator/signup/signUpValidator.ts
@@ -5,6 +5,7 @@ export const signUpValidator = withZod(
     z.object({
         email: z
             .string()
+            .min(1, { message: "メールアドレスを入力してください" })
             .email({ message: "メールアドレスを正しい形式で入力してください" }),
         password: z.string().min(4, { message: "パスワードは4文字以上入力してください" }),
     })
